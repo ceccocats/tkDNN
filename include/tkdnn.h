743c2dbfa48fd5b180cf4387a43cb980fc4c1046
Francesco Gatti <gattifrancesco@hotmail.it>
2017-06-29 12:50:27 +0000

    tkDNN library version 0.1

---
+++ include/tkdnn.h
@@ -0,0 +1,16 @@
+/**
+    This is the core header of the library, it should be used only this
+*/
+#include "Network.h"
+#include "Layer.h"
+
+namespace tkDNN {
+
+    /**
+        Return the tkDNN version
+    */
+    int getVersion() {
+
+        return 100;
+    }
+}
