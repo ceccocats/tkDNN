ab45c24efc4efca391834beca21576b27412f1e8
Francesco Gatti <gattifrancesco@hotmail.it>
2017-08-28 00:53:39 +0200

    check control ok

--- src/utils.cpp
+++ src/utils.cpp
@@ -86,7 +86,7 @@ int checkResult(int size, dnnType *data_d, dnnType *correct_d, bool device) {
 
     int diffs = 0;
     for(int i=0; i<size; i++) {
-        if(data_h[i] != data_h[i] && correct_h[i] != correct_h[i] && //nan control
+        if(data_h[i] != data_h[i] || correct_h[i] != correct_h[i] || //nan control
            fabs(data_h[i] - correct_h[i]) > eps) {
             diffs += 1;
             if(diffs == 1)
