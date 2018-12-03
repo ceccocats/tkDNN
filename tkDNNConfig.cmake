443179359d42ad2cc45c120f360c3cdf299fe2c3
Francesco Gatti <gattifrancesco@hotmail.it>
2018-12-03 22:04:04 +0100

    config

---
+++ tkDNNConfig.cmake
@@ -0,0 +1,10 @@
+find_package(CUDA REQUIRED)
+find_package(OpenCV REQUIRED)
+find_library(NVINFER NAMES nvinfer)
+if(NVINFER STREQUAL "NVINFER-NOTFOUND")
+    set(NVINFER_INCLUDES "/usr/local/nvidia/tensorrt/include/")
+    link_directories(/usr/local/nvidia/tensorrt/targets/x86_64-linux-gnu/lib/ 
+		     /usr/local/cuda/targets/x86_64-linux/lib/)
+endif()
+set(tkDNN_INCLUDE_DIRS ${CUDA_INCLUDE_DIRS} ${OPENCV_INCLUDE_DIRS} ${NVINFER_INCLUDES})
+set(tkDNN_LIBRARIES tkDNN kernels ${CUDA_LIBRARIES} ${CUDA_CUBLAS_LIBRARIES} -lcudnn -lnvinfer ${OpenCV_LIBS})
