110bf56dc41aec1bb7827e7212787d438b09d7f3
xavier <micaelaverucchi@gmail.com>
2020-02-21 11:14:52 +0100

    Fix include error
    Signed-off-by: xavier <micaelaverucchi@gmail.com>

--- include/sorting.h
+++ include/sorting.h
@@ -6,10 +6,6 @@
 #include <thrust/gather.h>
 #include <thrust/copy.h>
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include "opencv2/opencv.hpp"
 
 #include "tkdnn.h"
 
