5562f599a68b09b2cacf5434b9933b364c9f89c1
Davide Sapienza <sapienza.dav@gmail.com>
2020-04-08 10:22:03 +0200

    Fix the INT8 calibrator sintax
    Signed-off-by: Davide Sapienza <sapienza.dav@gmail.com>

--- include/tkDNN/Int8Calibrator.h
+++ include/tkDNN/Int8Calibrator.h
@@ -18,9 +18,17 @@
 #include "tkdnn.h"
 #include "utils.h"
 
-class Int8EntropyCalibrator : public nvinfer1::IInt8EntropyCalibrator{
+/*
+ * Int8EntropyCalibrator implements the INT8 calibrator to achieve the
+ * INT8 quantization. It uses a BatchStream stream to scroll through 
+ * images data. It also implements the calibration cache, a way to 
+ * save the calibration process results to reduce the running time: 
+ * the calibration process takes a long time.
+ */
+class Int8EntropyCalibrator : public nvinfer1::IInt8EntropyCalibrator {
 public:
-	Int8EntropyCalibrator(BatchStream& stream, int firstBatch, const std::string& calibTableFilePath, const std::string& inputBlobName, bool readCache = true);
+	Int8EntropyCalibrator(BatchStream& stream, int firstBatch, const std::string& calibTableFilePath, 
+							const std::string& inputBlobName, bool readCache = true);
 	virtual ~Int8EntropyCalibrator() { checkCuda(cudaFree(mDeviceInput)); }
 	int getBatchSize() const override { return mStream.getBatchSize(); }
 	bool getBatch(void* bindings[], const char* names[], int nbBindings) override;
@@ -29,7 +37,7 @@ public:
 
 private:
 	BatchStream mStream;
-	const std::string mCalibTableFilePath{nullptr};
+	const std::string mCalibTableFilePath{ nullptr };
 	const std::string mInputBlobName;
 	bool mReadCache{ true };
 
