e036c5da34710375c9a044ffe81fd8489282c78e
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-04-08 12:34:02 +0200

    Update download url
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

--- tests/yolo3_512/yolo3_512.cpp
+++ tests/yolo3_512/yolo3_512.cpp
@@ -10,7 +10,7 @@ int main() {
 
     // create yolo3 model
     std::string bin_path  = "../tests/yolo3_512";
-    downloadWeightsifDoNotExist("../tests/yolo3_512/layers/input.bin", bin_path, "https://cloud.hipert.unimore.it/s/R6qSLfFAF9F2ni9/download");
+    downloadWeightsifDoNotExist("../tests/yolo3_512/layers/input.bin", bin_path, "https://cloud.hipert.unimore.it/s/e7HfScx77JEHeYb/download");
     int classes = 80;
     tk::dnn::Yolo *yolo [3];
     #include "models/Yolo3.h"
