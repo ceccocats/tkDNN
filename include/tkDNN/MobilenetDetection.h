40456592fc6693ca224202a88d78bbaa20446cce
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-05-14 16:41:51 +0200

    Adapt detection classes to use batches, adapt demos, update README
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

--- include/tkDNN/MobilenetDetection.h
+++ include/tkDNN/MobilenetDetection.h
@@ -65,9 +65,9 @@ public:
     MobilenetDetection() {};
     ~MobilenetDetection() {}; 
 
-    bool init(const std::string& tensor_path, const int n_classes);
-    void preprocess(cv::Mat &frame);
-    void postprocess(const bool mAP=false);
+    bool init(const std::string& tensor_path, const int n_classes, const int n_batches=1);
+    void preprocess(cv::Mat &frame, const int bi=0);
+    void postprocess(const int bi=0,const bool mAP=false);
 };
 
 
