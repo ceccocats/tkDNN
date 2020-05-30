40aefba5074abd51f3a6c64f86d082808526ff76
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-05-30 16:57:53 +0200

    Add mish_yashas
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

--- src/kernels/activation_mish.cu
+++ src/kernels/activation_mish.cu
@@ -3,12 +3,30 @@
 
 #define MISH_THRESHOLD 20
 
-__device__ float tanh_activate_kernel(float x){return (2/(1 + expf(-2*x)) - 1);}
-__device__ float softplus_kernel(float x, float threshold = 20) {
+__device__ 
+float tanh_activate_kernel(float x){return (2/(1 + expf(-2*x)) - 1);}
+
+__device__ 
+float softplus_kernel(float x, float threshold = 20) {
     if (x > threshold) return x;                // too large
     else if (x < -threshold) return expf(x);    // too small
     return logf(expf(x) + 1);
 }
+
+
+
+__device__ 
+float mish_yashas(float x) { 
+    float e = __expf(x); 
+    if (x <= -18.0f) 
+        return x * e; 
+ 
+    float n = e * e + 2 * e; 
+    if (x <= -5.0f) 
+        return x * __fdividef(n, n + 2); 
+ 
+    return x - 2 * __fdividef(x, n + 2); 
+} 
 
 // https://github.com/digantamisra98/Mish
 // https://github.com/AlexeyAB/darknet/blob/master/src/activation_kernels.cu
@@ -16,7 +34,8 @@ __global__
 void activation_mish(dnnType *input, dnnType *output, int size) {
     int i = (blockIdx.x + blockIdx.y*gridDim.x) * blockDim.x + threadIdx.x;
     if (i < size) 
-        output[i] = input[i] * tanh_activate_kernel( softplus_kernel(input[i], MISH_THRESHOLD));    
+        // output[i] = input[i] * tanh_activate_kernel( softplus_kernel(input[i], MISH_THRESHOLD));    
+        output[i] = mish_yashas(input[i]);
 }
 
 /**
