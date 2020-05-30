15105e90d30e0dc6877a9bee7686f90a31731b38
Micaela Verucchi <micaelaverucchi@gmail.com>
2020-05-30 17:25:48 +0200

    Add parseType
    Signed-off-by: Micaela Verucchi <micaelaverucchi@gmail.com>

--- include/tkDNN/DarknetParser.h
+++ include/tkDNN/DarknetParser.h
@@ -9,9 +9,16 @@ namespace tk { namespace dnn {
         tk::dnn::dataDim_t dim;
         tk::dnn::Network *net = new tk::dnn::Network(dim);
 
-
     }
-
-
+        
+    std::string parseType(const std::string& line){
+            size_t start = line.find("[");
+            size_t end  = line.find("]");
+            if( start == std::string::npos || end == std::string::npos)
+                return "";
+            start++;
+            std::string type = line.substr(start, end-start);
+            return type;
+    }
 
 }}
