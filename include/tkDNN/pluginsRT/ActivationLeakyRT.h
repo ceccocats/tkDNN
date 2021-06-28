b86a93e85d0679a1e494d98481132eb33b94ab22
Micaela Verucchi <micaelaverucchi@gmail.com>
2021-06-28 23:23:15 +0200

    ActivationLeaky IpluginV2

--- include/tkDNN/pluginsRT/ActivationLeakyRT.h
+++ include/tkDNN/pluginsRT/ActivationLeakyRT.h
@@ -1,7 +1,7 @@
 #include<cassert>
 #include "../kernels.h"
 
-class ActivationLeakyRT : public IPlugin {
+class ActivationLeakyRT : public IPluginV2 {
 
 public:
 	ActivationLeakyRT() {
@@ -13,33 +13,33 @@ public:
 
 	}
 
-	int getNbOutputs() const override {
+	int getNbOutputs() const noexcept override {
 		return 1;
 	}
 
-	Dims getOutputDimensions(int index, const Dims* inputs, int nbInputDims) override {
+	Dims getOutputDimensions(int index, const Dims* inputs, int nbInputDims) noexcept override {
 		return inputs[0];
 	}
 
-	void configure(const Dims* inputDims, int nbInputs, const Dims* outputDims, int nbOutputs, int maxBatchSize) override {
-		size = 1;
-		for(int i=0; i<outputDims[0].nbDims; i++)
-			size *= outputDims[0].d[i];
-	}
+	//void configure(const Dims* inputDims, int nbInputs, const Dims* outputDims, int nbOutputs, int maxBatchSize) override {
+	//	size = 1;
+	//	for(int i=0; i<outputDims[0].nbDims; i++)
+	//		size *= outputDims[0].d[i];
+	//}
 
-	int initialize() override {
+	int initialize() noexcept override {
 
 		return 0;
 	}
 
-	virtual void terminate() override {
+	virtual void terminate() noexcept override {
 	}
 
-	virtual size_t getWorkspaceSize(int maxBatchSize) const override {
+	virtual size_t getWorkspaceSize(int maxBatchSize) const noexcept override {
 		return 0;
 	}
 
-	virtual int enqueue(int batchSize, const void*const * inputs, void** outputs, void* workspace, cudaStream_t stream) override {
+	virtual int enqueue(int32_t batchSize, void const *const *inputs, void *const *outputs, void *workspace, cudaStream_t stream) noexcept override {
 
 		activationLEAKYForward((dnnType*)reinterpret_cast<const dnnType*>(inputs[0]), 
 											reinterpret_cast<dnnType*>(outputs[0]), batchSize*size, stream);
@@ -47,11 +47,11 @@ public:
 	}
 
 
-	virtual size_t getSerializationSize() override {
+	virtual size_t getSerializationSize() const noexcept override {
 		return 1*sizeof(int);
 	}
 
-	virtual void serialize(void* buffer) override {
+	virtual void serialize(void* buffer) const noexcept override {
 		char *buf = reinterpret_cast<char*>(buffer),*a=buf;
 		tk::dnn::writeBUF(buf, size);
 		assert(buf == a + getSerializationSize());
