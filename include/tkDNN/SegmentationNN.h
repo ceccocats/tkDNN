d2ce966c46877495a1d9fa83edab45620173ec4e
perseusdg <f20180523@goa.bits-pilani.ac.in>
2021-08-04 12:37:58 +0530

    fix return type for bool functions

--- include/tkDNN/SegmentationNN.h
+++ include/tkDNN/SegmentationNN.h
@@ -183,6 +183,7 @@ class SegmentationNN {
 
             checkCuda(cudaMemcpyAsync(mean_d, mean.data(), mean.size() * sizeof(float), cudaMemcpyHostToDevice, netRT->stream));
             checkCuda(cudaMemcpyAsync(stddev_d, stddev.data(), stddev.size() * sizeof(float), cudaMemcpyHostToDevice, netRT->stream));
+            return true;
 
         }
         
